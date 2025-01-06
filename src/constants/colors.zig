f477a8d2c3534a97a2efc5ff1640092321408aaa
Karolis Balčiūnas <vagabaliukas@gmail.com>
2025-01-07 01:01:13 +0200

    Draw the initial window

---
+++ src/constants/colors.zig
@@ -0,0 +1,3 @@
+const rl = @import("raylib");
+
+pub const dark_blue = rl.Color{ .r = 44, .g = 44, .b = 127, .a = 255 };
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,19 @@
 const std = @import("std");
+const rl = @import("raylib");
+const constants = @import("constants");
+const colors = constants.colors;
 
-pub fn main() !void {}
+pub fn main() !void {
+    rl.initWindow(300, 600, "Zetris");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+    rl.setExitKey(rl.KeyboardKey.null);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(colors.dark_blue);
+    }
+}
